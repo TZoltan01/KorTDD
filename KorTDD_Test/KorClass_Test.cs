cec65048dfa8fdc0ffa323d629820243249fb546
TZoltan01 <topcsiovzoltan0402@gmail.com>
2025-02-19 11:55:56 +0100

    Add project files.

---
+++ KorTDD_Test/KorClass_Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using KorTDD;
+
+namespace KorTDD_Test
+{
+    [TestClass]
+    public class KorClass_Test
+    {
+        [TestMethod]
+        public double Kerulet_Test()
+        {
+
+        }
+    }
+}
---
+++ KorTDD_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KorTDD_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KorTDD_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("72c700e2-bcde-4b0c-8a6c-4c542d18a501")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
